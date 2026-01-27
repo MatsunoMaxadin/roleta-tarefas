9a95ac05aec22b5b7223f5d75db306759526a5e5
MatsunoMaxadin <pedropaz4.511@gmail.com>
2026-01-26 23:25:15 -0300

    Criando a interface para o projeto

--- Menu.java
+++ Menu.java
@@ -1,19 +1,67 @@
 import javax.swing.JFrame;
 import javax.swing.JButton;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.FlowLayout;
 
-public class Menu{
-	private Jlabel titulo = new JLabel("Bem-vindo à Roleta de Tarefas!");
-	private JButton inicializar = new JButton("Inicializar");
+import java.util.ArrayList;
 
+public class Menu extends JFrame{
+	private JLabel titulo = new JLabel("Bem-vindo à Roleta de Tarefas!");
+	private JButton adicionar = new JButton("Adicionar Tarefa");
+	private JButton roletar = new JButton("Girar a roleta");
+	private boolean listaExiste = false;
+	private	ArrayList <String> tarefas = new ArrayList<String>();
 	public Menu(){
 		super("Roleta de tarefas");
+
 		setLayout( new FlowLayout() );
 
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setSize(300,300);
+		setVisible(true);
+
 		add(titulo);
+		
+		adicionar.addActionListener(new ActionListener(){
+			public void actionPerformed(ActionEvent evento){
+				if(evento.getSource() == adicionar){
+					String op = "s";
+					String tarefa;
+					while(op.equals("s")){
+						tarefa = JOptionPane.showInputDialog("Digite uma tarefa: ");
+						tarefas.add(tarefa);
+						op = JOptionPane.showInputDialog("Deseja inserir outra tarefa? (s/n)");
+						
+					}
+					listaExiste = true;
+
+				}
+			}
+		
+		});
+
+		add(adicionar);
+
+		roletar.addActionListener(new ActionListener(){
+			public void actionPerformed(ActionEvent evento){
+				if (evento.getSource() == roletar){
+					if(listaExiste){
+						Roleta roleta = new Roleta(tarefas);
+						JOptionPane.showMessageDialog(null, String.format("Tarefa escolhida pro dia: %s", roleta.girarRoleta())); 
+					} else {
+						JOptionPane.showMessageDialog(null, "Lista de tarefas não encontrada! Por favor, crie uma.");
+					}		
+				}
+			}
+		
+		});
+
+		add(roletar);
+
 
 		
 	}
